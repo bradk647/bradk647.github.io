0b03ccb560bd7c6ff7ded5f8b9a5fc39f736e2bf
bradk647 <bradk647@newschool.edu>
2015-02-23 22:48:18 +0000

    message ...

---
+++ bunny.js
@@ -0,0 +1,24 @@
+$();
+
+//variables
+// var name = 'Brain';
+// var height = '6.1';
+
+//string 
+//boolean 
+	//0-false
+	//1-true
+		//(=)-setting
+		//(==) - processing/computing
+//array
+	//0 - is the base
+		//0,1,2 etc. 
+
+var dresser = ['socks', 'shirts', 'boots']
+
+say_hi('Vivian');
+
+function say_hi(name) {
+	alert('Hello ' + name);
+
+}
