7bfb13cf6c7e0b114bcc284da4f10bb216f10faa
bradk647 <bradk647@newschool.edu>
2015-04-23 04:38:36 +0000

    message ...

---
+++ tarantinoscript.js
@@ -0,0 +1,69 @@
+var cats = ["http://38.media.tumblr.com/0ad73eb6c529aa667293644f7fe70b12/tumblr_mf7ug7T2ks1r2vbx0o1_500.gif","https://33.media.tumblr.com/18f53ea140772edce9cce290872db1f8/tumblr_mx05qj4gef1t0g055o2_500.gif","https://38.media.tumblr.com/ef9a2f5277529715711f1c78d676beab/tumblr_mjz89aKpHR1s8r067o1_500.gif","http://31.media.tumblr.com/27c9aa12fbf46187b980891bacb2c471/tumblr_n660rqJ0kL1qdtn3qo1_500.gif", "https://38.media.tumblr.com/532b0e5062033b1f1580bd71c5244ff8/tumblr_mwnhyhJ0Wh1r04g55o2_500.gif","http://media1.giphy.com/media/5WOQrpyJLZrSo/giphy.gif", "http://media.giphy.com/media/ozXTrqRgqFcly/giphy.gif","","http://media.giphy.com/media/5M5RQkEowc25y/giphy.gif", "http://media.giphy.com/media/GapkxfFrjPD5S/giphy.gif", "http://media.giphy.com/media/108FYAUOfvmfRe/giphy.gif","http://media.giphy.com/media/Qqq8b2K5qsg8M/giphy.gif","http://media.giphy.com/media/G5iBU0wPp9QKA/giphy.gif", "http://media.giphy.com/media/kL2qaj4nckOqI/giphy.gif", "http://media.giphy.com/media/12DiWqCYCAlyeY/giphy.gif","http://media2.giphy.com/media/D4QLJVmdHB44g/giphy.gif", "http://media0.giphy.com/media/cGZ9I3ZYcmFoY/giphy.gif", "http://media.giphy.com/media/rZLGH32X4Ei0o/giphy.gif", "http://media.giphy.com/media/Ua48yqUY0x5uM/giphy.gif", "http://media.giphy.com/media/EkF6cRec0rTXO/giphy.gif", "http://media.giphy.com/media/VZ0F6702VrfIA/giphy.gif", "http://media3.giphy.com/media/rpdPBecp8imfS/giphy.gif.", "http://media.giphy.com/media/pNfyBGF0rwsQ8/giphy.gif", "http://media0.giphy.com/media/2PKiy95Iq819K/giphy.gif", "http://media.giphy.com/media/W7dpMMqe8q26k/giphy.gif", "http://media.giphy.com/media/Xm3z6pU2RE96o/giphy.gif", "https://38.media.tumblr.com/6cc80e8c6d6e3bca5f00d707f6878b60/tumblr_mlod3dO7Nc1rd7s7yo1_500.gif","http://25.media.tumblr.com/423cb777516c596e85ca8669f5fbcd76/tumblr_mk45bvSr501s92gd2o2_500.gif", "http://31.media.tumblr.com/tumblr_m520vupooe1qg7zl2o1_r1_500.gif", "http://24.media.tumblr.com/e0f47609fe7e7d3823575ae7c711e48f/tumblr_mh4y0r8nlv1r0k2r8o2_r1_500.gif", "http://24.media.tumblr.com/4d4239dc5cb3f10ed9e955175f7d3b31/tumblr_mjz2ccZ1Rv1s5w098o1_500.gif", "https://33.media.tumblr.com/773f1146f74c374e9df57e5fcca709d2/tumblr_mn01wlhprW1rgyrl3o1_500.gif", "https://33.media.tumblr.com/067d9478a9b61e3dbcec81a3f92150b4/tumblr_mpteg7bxsm1reey4zo2_500.gif", "http://38.media.tumblr.com/tumblr_mclglkVm9L1qi6wtco2_500.gif", "http://33.media.tumblr.com/ed4c2f2262943d8dc8b8ad6bdf2239f0/tumblr_na95wnPAks1qcga5ro1_500.gif", "http://31.media.tumblr.com/227fa1451360843296f3534bb35b9934/tumblr_mt6ny0Rz3U1rw7rajo1_500.gif", "http://thefilmexperience.net/storage/melanie%20jackie%20brown.gif?__SQUARESPACE_CACHEVERSION=1390870352753", "http://25.media.tumblr.com/tumblr_m9tzsfCpeb1rq9nqbo1_500.gif", "https://33.media.tumblr.com/fdf0dc78728bc17398265ce109b7b1b2/tumblr_mkftp2mIr91qgt6uyo1_500.gif", "http://33.media.tumblr.com/ade1f4b12e81b4391312c5fba42ddaa5/tumblr_inline_n8v4lmpjkZ1rqg5v6.gif", "http://31.media.tumblr.com/f0332c1226ca4af2ac7a65448482ffb3/tumblr_mzbsuywqTL1skzvedo1_500.gif", "http://data3.whicdn.com/images/63454036/original.gif"];
+
+var poems = ["If you just love movies enough, you can make a good one.","When people ask me if I went to film school I tell them, 'no, I went to films.","I steal from every movie ever made.", "I was kind of excited about going to jail the first time and I learnt some great dialogue.", "Violence is one of the most fun things to watch.", "I loved history because to me, history was like watching a movie.", "I have loved movies as the number one thing in my life so long that I can't ever remember a time when I didn't.", "The good ideas will survive.", "I want to top expectations. I want to blow you away.", "I couldn't spell anything. I couldn't remember anything, but I could go to a movie and I knew who starred in it, who directed it, everything.", "My plan is to have a theatre in some small town or something and I'll be manager. Ill be the crazy old movie guy.", "I don't think Pulp Fiction is hard to watch at all.", "All my movies are achingly personal.", "I've always thought John Travolta is one of the greatest movie stars Hollywood has ever produced.", "Sergio Leone was a big influence on me because of the spaghetti westerns.", "I look at 'Death Proof' and realize I had too much time.", "I actually think one of my strengths is my storytelling.", "I'm a historian in my own mind.", "If I wasn't a film-maker, I'd be a film critic. It's the only thing I'd be qualified to do.", "To me, America is just another market."];
+
+var selectedpoem = false;
+
+$(".go-button").click(function() {
+  
+  var nextpoem = poems[Math.floor(Math.random()*poems.length)];
+  
+  while (selectedpoem == nextpoem) {
+    nextpoem = poems[Math.floor(Math.random()*poems.length)];
+  }
+  
+  selectedpoem = nextpoem;
+  
+  $(".poem-holder").html(nextpoem);
+
+  $(".cat-holder").html('<img src="' + cats[Math.floor(Math.random()*cats.length)]+ '">');
+  
+  var newheight = (Math.floor(Math.random() * 100) + 100) + "px";
+
+$(".line").css("height", newheight);
+  
+  
+for (var increment = 0; increment < 100; increment++) {
+  $("body").append("<div class='dot'></div>"); 
+}
+
+$(".dot").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+  
+  $(".dot1").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+    $(".dot2").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+  
+  $(".dot3").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+    $(".dot4").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+  
+  $(".dot5").each(function() {
+  var newtop = Math.floor(Math.random()*100) + "%";
+  var newleft = Math.floor(Math.random()*100) + "%";
+  $(this).css("top",newtop);
+  $(this).css("left",newleft);
+});
+});
+ });
+  });
+  });
+  });
+});
